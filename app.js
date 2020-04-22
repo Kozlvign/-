0bf617f1bce7cd6dda13ab4f4acbb8c8b6527fa1
Kozlvign <63605496+Kozlvign@users.noreply.github.com>
2020-04-22 09:30:30 -0300

    Adding items to controller

--- app.js
+++ app.js
@@ -19,6 +19,28 @@ var budgetController = (function() {
             exp: 0,
             inc: 0
         }
+    };
+
+    return {
+        addItem: function(type, des, val) {
+            var newItem, ID;
+
+            if (data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+
+            if (type === 'exp') {
+                newItem = new Expense(ID, des, val);
+            } else if (type === 'inc') {
+                newItem = new Income(ID, des, val);
+            }
+
+            data.AllItems[type].push(newItem);
+            return newItem;
+            
+        }
     }
 
 })();
@@ -60,8 +82,11 @@ var controller = (function(budgetCtrl, UICtrl) {
     };
 
     var ctrlAddItem = function () {
-        var input = UICtrl.getInput();
-        console.log(input)
+        var input, newItem;
+        
+        input = UICtrl.getInput();
+        
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
     };
 
